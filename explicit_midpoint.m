450eb17858d204cfa68b27c0efc1742ffa212cab
Addison Fisher <afisher@olin.edu>
2025-10-19 21:39:51 -0400

    updated method files

--- explicit_midpoint.m
+++ explicit_midpoint.m
@@ -1,8 +1,6 @@
-function EMBT = explicit_midpoint
-    EMBT = struct();
-
-    EMBT.C = [0,   1/2];
-    EMBT.A = [0,   0
+function [EMBT_C, EMBT_A, EMBT_B] = explicit_midpoint
+    EMBT_C = [0,   1/2];
+    EMBT_A = [0,   0
               1/2, 0];
-    EMBT.B = [0,   1];
+    EMBT_B = [0,   1];
 end
---
+++ forward_euler.m
@@ -0,0 +1,5 @@
+function [FEBT_C, FEBT_A, FEBT_B] = forward_euler()
+    FEBT_A = 0;
+    FEBT_B = 1;
+    FEBT_C = 0;
+end
