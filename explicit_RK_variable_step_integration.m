6ff9f977e06f22e6f9a64e581b00196e20ca8921
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-11-03 23:25:49 -0500

    exp var final

--- explicit_RK_variable_step_integration.m
+++ explicit_RK_variable_step_integration.m
@@ -1,28 +1,6 @@
-%Runs numerical integration arbitrary RK method using variable time steps
+function [t_list,X_list,h_avg,num_evals,step_failure_rate] = explicit_RK_variable_step_integration ...
+    (rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
 
-%INPUTS:
-%rate_func_in: the function used to compute dXdt. rate_func_in will
-% have the form: dXdt = rate_func_in(t,X) (t is before X)
-%tspan: a two element vector [t_start,t_end] that denotes the integration endpoints
-%X0: the vector describing the initial conditions, X(t_start)
-%h_ref: the desired value of the average step size (not the actual value)
-%BT_struct: a struct that contains the Butcher tableau
-% BT_struct.A: matrix of a_{ij} values
-% BT_struct.B: vector of b_i values
-% BT_struct.C: vector of c_i values
-
-%p: how error scales with step size (error = k*hˆp)
-%error_desired: the desired local truncation error at each step
-
-%OUTPUTS:
-%t_list: the vector of times, [t_start;t_1;t_2;...;.t_end] that X is approximated at
-%X_list: the vector of X, [X0';X1';X2';...;(X_end)'] at each time step
-%h_avg: the average step size
-%num_evals: total number of calls made to rate_func_in during the integration
-
-function [t_list,X_list,h_avg, num_evals, step_failure_rate] = explicit_RK_variable_step_integration ...
-(rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
-   
     failed_steps = 0;
     attempted_steps = 0;
 
@@ -35,30 +13,37 @@ function [t_list,X_list,h_avg, num_evals, step_failure_rate] = explicit_RK_varia
     X_list = X0'; 
     
     total_num_evals = 0;
-    fail_steps = 0;
-    step_attemps = 0;
 
     while t_list(end) < t_end
         current_t = t_list(end);
-        %get current state vector
         current_X = X_list(end, :)'; 
 
-        %get next state
-        [next_X, step_evals, failed_steps, attempted_steps, h_next, redo] = explicit_RK_variable_step(rate_func_in, current_t, current_X, h_curr, BT_struct, p,error_desired);
-        
-        h_curr = h_next;
-        fail_steps = fail_steps + failed_steps;
-        step_attemps = attempted_steps + step_attemps;
+        % Adjust step to not overshoot t_end
+        if current_t + h_curr > t_end
+            h_curr = t_end - current_t;
+        end
 
-        %update the total count of rate function evaluations
-        total_num_evals = total_num_evals + step_evals;
+        redo = true;
+        while redo
+            attempted_steps = attempted_steps + 1;
 
-        t_list(end + 1, 1) = current_t + h_curr;
-        X_list(end + 1, :) = next_X';
-
+            [next_X, step_evals, failed, ~, h_next, redo] = ...
+                explicit_RK_variable_step(rate_func_in, current_t, current_X, h_curr, BT_struct, p, error_desired);
+            
+            total_num_evals = total_num_evals + step_evals;
+            
+            if redo
+                failed_steps = failed_steps + failed;
+                h_curr = h_next;  
+            else
+                t_list(end + 1, 1) = current_t + h_curr;
+                X_list(end + 1, :) = next_X';
+                h_curr = h_next;  
+            end
+        end
     end
-    step_failure_rate = failed_steps/attempted_steps;
-    disp(step_failure_rate);
+
+    step_failure_rate = failed_steps / attempted_steps;
     num_evals = total_num_evals;
     h_avg = mean(diff(t_list));
 end
